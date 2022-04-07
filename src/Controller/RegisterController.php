80a84072cf3a441f66e4f924cc35ba3f42e19361
Alexis Ferreira <alexisferreira@MacBook-Pro-de-Alexis.local>
2022-04-07 17:29:37 +0200

    MAJ ajout form inscription AVEC hash mdp

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
@@ -20,17 +21,27 @@ class RegisterController extends AbstractController
 
 
     #[Route('/inscription', name: 'app_register')]
-    public function register(Request $request): Response
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
 
         $user = new User();
-        $form = $this->createForm(RegisterType::class, $user);
 
+        $form = $this->createForm(RegisterType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
 
             $user = $form->getData();
+
+            $plaintextPassword = $user->getPassword(); // Password en clair
+
+            // Hash du password
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+
+            $user->setPassword($hashedPassword);
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
